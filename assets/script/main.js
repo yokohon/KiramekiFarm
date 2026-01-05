64346d9416071e2c38014a34a7a2307b3a929872
yoko.hon <kyogranatus@jcom.home.ne.jp>
2026-01-05 21:12:49 +0900

    price-sp

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,3 +9,22 @@ $('.hamburger-button').on('click', function () {
 $(window).on('resize', function () {
   hamburger.removeClass('hamburger-menu-container-active');
 });
+
+// スリック
+$('.gallery-box').slick({
+  infinite: true,
+  slidesToShow: 3,      // PCで3枚表示
+  slidesToScroll: 1,
+  arrows: true,         // 矢印ナビゲーションON
+  dots: true,           // 下部のドットON
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 768,  // スマホ時
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
